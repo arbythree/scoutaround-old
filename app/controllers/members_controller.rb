6822b8587d03e768e57c43fa1b2a532a41e44055
Ray Bradley <ray@bradleys.io>
2018-05-15 06:04:39 -0400

    Implemented client-side name search

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,11 +1,12 @@
 class MembersController < AuthenticatedController
   before_action :find_unit
+  before_action :find_member, only: :show
 
   def index
     if (params[:show] || '').split(',').include? 'inactive'
-      @memberships = @unit.memberships
+      @memberships = @unit.memberships.includes(:person).order('people.first_name')
     else
-      @memberships = @unit.memberships.active
+      @memberships = @unit.memberships.active(include: :person).order(:first_name)
     end
     @members = @memberships.map { |m| m.person }
   end
@@ -21,10 +22,17 @@ class MembersController < AuthenticatedController
     end
   end
 
+  def show
+  end
+
   private
 
   def find_unit
     @unit = @current_person.units.find(params[:unit_id])
+  end
+
+  def find_member
+    @member = @unit.members.find(params[:id])
   end
 
   def member_params
