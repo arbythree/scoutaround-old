357c82f3f90997711cd35387507d01e626609ace
Ray Bradley <ray@sema4genomics.com>
2018-05-31 12:01:32 -0400

    More logging...WTF is wrong?

--- app/controllers/unit_context_controller.rb
+++ app/controllers/unit_context_controller.rb
@@ -3,6 +3,7 @@ class UnitContextController < AuthenticatedController
 
   def find_unit
     return unless params[:unit_id].present?
+    logger.info "Current user is #{ @current_user }"
     @unit = @current_user.units.find(params[:unit_id])
     @current_user_is_admin = @unit.role_for(user: @current_user) == 'admin'
   end
