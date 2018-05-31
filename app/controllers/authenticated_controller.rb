994b451eb316a0151c7c6c1de7efbfd868ee449b
Ray Bradley <ray@sema4genomics.com>
2018-05-31 12:16:02 -0400

    Hacking auth to see if we can get it going again

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -1,8 +1,10 @@
 class AuthenticatedController < ApplicationController
   before_action :authenticate_user!
   before_action :set_current_user
+  skip_before_action :verify_authenticity_token
 
   def set_current_user
+    logger.info "Devise current_user is #{ current_user }"
     @current_user = current_user
     logger.info "Set current user: #{ @current_user }"
   end
