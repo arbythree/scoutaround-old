6af2a797ebf15ee4c1e9b7e7a6c6c0f02ff7f867
Ray Bradley <ray.bradley@gmail.com>
2018-09-14 07:58:37 -0400

    Event and announcement styling & scripting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < AuthenticatedController
   def index
-    # unless @current_user.multiple_units?
-    redirect_to unit_events_path(unit_id: @current_user.units.first.id)
-    # end
+    redirect_to unit_path(@current_user.units.first)
   end
 end
